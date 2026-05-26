b730533f6d2cad7ca0cbf35be53941fcee5046cc
Chirayu Khatri <chirayu.khatri@yahoo.co.in>
2026-05-26 12:47:20 +0000

    Expand agent sudoers for runtime commands

--- scripts/install.sh
+++ scripts/install.sh
@@ -263,7 +263,7 @@ do
 done
 
 cat > "${SUDOERS_FILE}" <<EOF
-${SERVICE_USER} ALL=(root) NOPASSWD: /usr/bin/systemctl, /usr/bin/apt-get, /usr/bin/install, /usr/bin/rm, /usr/bin/journalctl
+${SERVICE_USER} ALL=(root) NOPASSWD: /usr/bin/systemctl, /usr/bin/apt-get, /usr/bin/install, /usr/bin/rm, /usr/bin/journalctl, /usr/bin/chown, /usr/bin/chmod, /usr/bin/setfacl, /usr/bin/mosquitto_passwd, /usr/bin/mosquitto_sub, /usr/bin/python3
 EOF
 chmod 0440 "${SUDOERS_FILE}"
 
